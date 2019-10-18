0d4c9f5638a010d3cf88427405b7d402adfd90a1
rkaku <greenapple728@icloud.com>
2019-10-18 17:39:23 +0900

    why jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <h1>
-      Hello, World!
-    </h1>
-  );
+class App extends Component {
+  render() {
+    return <h1>Hello, World!</h1>;
+  };
 }
+
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello, React!"
+//     );
+//   };
+// }
 
 export default App;
