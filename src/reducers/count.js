cc01a9da56a3e4d152aea06f1d0cdcc115b02bfd
rkaku <greenapple728@icloud.com>
2019-10-18 19:30:17 +0900

    Reducer

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,13 +1,26 @@
+// Actions
 import { INCREMENT, DECREMENT } from '../actions';
 
+
+// Initial State
 const initialState = { value: 0 };
 
+
+// Reducer
 export default (state = initialState, action) => {
+
+  // Action -> State
   switch (action.type) {
+
+    // Plus One
     case INCREMENT:
       return { value: state.value + 1 };
+
+    // Minus One
     case DECREMENT:
       return { value: state.value - 1 };
+
+    // Default
     default:
       return state;
   }
