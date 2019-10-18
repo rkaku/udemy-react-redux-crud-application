fb2753e3275c8bbcbf7486bfcc504e9c0450d3d0
rkaku <greenapple728@icloud.com>
2019-10-18 17:58:12 +0900

    more jsx

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import { Component } from 'react';
 
 class App extends Component {
   render() {
+    // const greeting = "Hello";
+    // const dom = <h1 className="foo">{ greeting }, World!</h1>;
+    // return dom;
+    // const input = <input type="button" value="Click" onClick={ () => { console.log("Clicked!") } } />
+    // const input = <input type="text" onChange={ () => { console.log("Changed!") } } />;
+    // return input;
     return (
       <React.Fragment>
-        <label htmlFor="Click">Foo</label>
-        <input id="Click" value="Bar" type="button" onClick={() => console.log('I am clicked!')} />
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={ () => { console.log("Changed!") } } />
       </React.Fragment>
-    )
-  }
+    );
+  };
 }
 
-// function App() {
-//   return (
-//     <h1>
-//       Hello, World!
-//     </h1>
-//   );
-// }
 
 export default App;
