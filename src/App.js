e959e22d925062d469c6a6049d29bead2e2b7f26
rkaku <greenapple728@icloud.com>
2019-10-30 07:57:07 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import React, { useCallback } from "react"
+import React, { useCallback, useMemo } from "react"
+
+function User(props) {
+  console.log(props)
+  return (
+    <>
+      <div>{ props.user.name }</div>
+      <div>{ props.user.age }</div>
+    </>
+  )
+}
 
 function App(props) {
+  console.log(props)
   return (
     <>
       <h1>Hello, World!</h1>
+      {
+        props.users.map(user => (
+          <User key={ user.id } user={ user } />
+        ))
+      }
       <input onChange={ props.onClickHandle }></input>
     </>
   )
 }
 
 function Container() {
-  const onClickHandle = useCallback(() => {
-    console.log("I am clicked!")
+  const users = useMemo(() => {
+    return [
+      {
+        id: 1,
+        name: "Taro",
+        age: 10
+      },
+      {
+        id: 2,
+        name: "Naomi",
+        age: 5
+      }
+    ]
   }, [])
-  return <App onClickHandle={ onClickHandle } />
+  const onClickHandle = useCallback(() => { console.log("I am clicked!") }, [])
+  return <App
+    users={ users }
+    onClickHandle={ onClickHandle } />
 }
 
 export default Container
