e1d0a4b77a3a48693a8baddb6dec929c98503fd2
rkaku <greenapple728@icloud.com>
2019-10-30 11:51:00 +0900

    reducer

--- src/actions/reducers/App.js
+++ src/actions/reducers/App.js
@@ -1,2 +1,20 @@
 import types from '../types/App'
 
+function initialState() {
+  return {
+    value: 0
+  }
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case types.INCREMENT:
+      return { ...state, value: state.value + 1 }
+    case types.DECREMENT:
+      return { ...state, value: state.value - 1 }
+    default:
+      return state
+  }
+}
+
+export { initialState, reducer }
---
+++ src/actions/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux'
+import { reducer } from '../reducers/App'
+
+export default combineReducers({ reducer })
