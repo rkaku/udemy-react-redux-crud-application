75bc2c3035c1fc9b42728cb78083fe7929434e64
rkaku <greenapple728@icloud.com>
2019-11-02 19:34:23 +0900

    implement redux devtools

--- src/redux/reducers/store.js
+++ src/redux/reducers/store.js
@@ -1,7 +1,20 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux'
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { reducer } from "redux-form"
 import events from './events'
 
 const rootReducer = combineReducers( { form: reducer, events } )
-export const store = createStore( rootReducer, applyMiddleware( thunk ) )
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    :
+    compose
+
+const enhancer = composeEnhancers( applyMiddleware( thunk ) )
+
+export const store = createStore( rootReducer, enhancer )
+
+
+// export const store = createStore( rootReducer, applyMiddleware( thunk ) )
